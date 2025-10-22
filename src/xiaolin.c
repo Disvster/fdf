dd776b9fce649ac570b5ec0d6813f29ad7579a4e
Manuel Maria Carreiro Couto <manmaria@c1r10s5.42porto.com>
2025-10-22 18:27:37 +0100

    updated lib

--- src/xiaolin.c
+++ src/xiaolin.c
@@ -1,21 +1,44 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_abs.c                                           :+:      :+:    :+:   */
+/*   xiaolin.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: manmaria <manmaria@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/22 16:47:27 by manmaria          #+#    #+#             */
-/*   Updated: 2025/10/22 18:27:21 by manmaria         ###   ########.fr       */
+/*   Created: 2025/10/22 16:50:09 by manmaria          #+#    #+#             */
+/*   Updated: 2025/10/22 18:26:43 by manmaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../incs/libft.h"
+#include "../incs/fdf.h"
 
-int	ft_abs(int nb)
+float ft_abs_float(float nb)
 {
 	if (nb >= 0)
 		return (nb);
 	else
 		return (nb * -1);
+}
+
+int	i_partof_number(float nb)
+{
+	return ((int)nb);
+}
+
+float	f_partof_number(float nb)
+{
+	if (nb > 0)
+		return (nb - i_partof_number(nb));
+	else
+		return (- nb - i_partof_number(nb));
+}
+
+#include <stdio.h>
+int	main(void)
+{
+	float i = -0.80;
+
+	printf("i = %.2f\ni_partof_number(i) = %d\n", i, i_partof_number(i));
+	printf("f_partof_number(i) = %.2f\n", f_partof_number(i));
+	return (0);
 }
