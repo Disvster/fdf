bc7f27f9d7fd18e72a54a35cf17bbf8279f4c7e2
Manuel Maria Carreiro Couto <manmaria@c1r10s5.42porto.com>
2025-10-26 21:36:29 +0000

    coordinates are transformed

--- src/main.c
+++ src/main.c
@@ -63,12 +63,11 @@ int	main(int ac, char **av)
 	mlx = mlx_init(); //display init
 	if (!mlx)
 		return (1);
-	mlx_win = mlx_new_window(mlx, 500, 500, "FdF");
-	img.img = mlx_new_image(mlx, 500, 500);
+	mlx_win = mlx_new_window(mlx, 1000, 1000, "FdF");
+	img.img = mlx_new_image(mlx, 1000, 1000);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
 
 	transform(&data);
-	print_img(&data);
 	draw_line(&data, &img);
 
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
--- src/project.c
+++ src/project.c
@@ -18,28 +18,47 @@ void	init_view(t_data *data)
 	data->view.beta = 0;
 	data->view.gamma = 0;
 	data->view.zoom = 25;
-	data->view.scale = 1;
-	data->view.angle = 0.523599;
+	data->view.scale = 5.0;
+	// data->view.angle = 0.523599;
+	data->view.angle = M_PI / 4;
 }
 
-void	project(t_data *data, t_point *points)
-{
-	points->display[0] = abs((int)((points->x - points->y) * cos(data->view.angle)));
-	points->display[1] = abs((int)((points->x + points->y) * sin(data->view.angle) - points->z));
-	ft_printf("disp x = %d\n", points->display[0]);
-	ft_printf("disp y = %d\n", points->display[1]);
-}
 // x = (int)((point->world_3d[0] - point->world_3d[1])
 // 			* cos(data->view.angle))
 // y = (int)((point->world_3d[0] + point->world_3d[1])
 // 			* sin(data->view.angle) - point->world_3d[2]);
+void	project(t_data *data, t_point *points)
+{
+	points->display[0] = (int)((points->x - points->y)
+		* cos(data->view.angle));
+	points->display[1] = (int)((points->x + points->y)
+		* sin(data->view.angle) - points->z);
+}
 
 void	transform(t_data *data)
 {
 	int	i;
+	int	min_x;
+	int	min_y;
 
 	i = -1;
+	min_x = INT_MAX;
+	min_y = INT_MAX;
 	init_view(data);
 	while (++i < data->map.points_total)
 		project(data, &data->points[i]);
+	i = -1;
+	while (++i < data->map.points_total)
+	{
+		if (data->points[i].display[0] < min_x)
+			min_x = data->points[i].display[0];
+		if (data->points[i].display[1] < min_y)
+			min_y = data->points[i].display[1];
+		data->points[i].display[0] -= min_x;
+		data->points[i].display[1] -= min_y;
+		data->points[i].display[0] = (int)(data->points[i].display[0]
+		* data->view.scale);
+		data->points[i].display[1] = (int)(data->points[i].display[1]
+		* data->view.scale);
+	}
 }
