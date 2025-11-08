c92777638f283042154c19b0748c35d6214139db
Manuel Maria Carreiro Couto <manmaria@c1r10s5.42porto.com>
2025-11-08 23:22:18 +0000

    added other fdf for reference and inspo

---
+++ other_repos/fdf_felipe/libft/ft_count_words.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_count_words.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: feazeved <feazeved@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/12 21:46:09 by feazeved          #+#    #+#             */
+/*   Updated: 2025/10/12 21:46:42 by feazeved         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_count_words(char *str, char sep)
+{
+	int	i;
+	int	words;
+
+	i = 0;
+	words = 0;
+	while (str && str[i])
+	{
+		while (str[i] == sep)
+			i++;
+		if (str[i])
+			words++;
+		while (str[i] && str[i] != sep)
+			i++;
+	}
+	return (words);
+}
