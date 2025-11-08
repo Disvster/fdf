9747f06a01032e1655b1d04909bb1076761fe5d7
Manuel Maria Carreiro Couto <manmaria@c1r10s5.42porto.com>
2025-11-08 23:19:50 +0000

    added other fdf for reference and inspo

---
+++ other_repos/fdf_felipe/test_fts.c
@@ -0,0 +1,54 @@
+#include "../incs/fdf.h"
+
+void	print_map(t_data *data)
+{
+	int	y;
+	int	x;
+	int	i;
+
+	y = 0;
+	x = 0;
+	i = 0;
+	while (y < data->map.height)
+	{
+		while (x < data->map.width)
+		{
+			ft_printf(" x%d,y%d,z%d",//,c%d ",
+			 data->points[i].x, data->points[i].y, data->points[i].z);//, data->points[i].color);
+			x++;
+			i++;
+			if (x == data->map.width)
+				ft_printf("\n");
+		}
+		x = 0;
+		y++;
+	}
+	ft_printf("\ntotal nbr of points -> %d\n", data->map.points_total);
+	ft_printf("total nbr of coordinates printed -> %d\n", i);
+}
+void	print_img(t_data *data)
+{
+	int	y;
+	int	x;
+	int	i;
+
+	y = 0;
+	x = 0;
+	i = 0;
+	while (y < data->map.height)
+	{
+		while (x < data->map.width)
+		{
+			ft_printf(" x%d,y%d ",
+			 data->points[i].display[0], data->points[i].display[1]);
+			x++;
+			i++;
+			if (x == data->map.width)
+				ft_printf("\n");
+		}
+		x = 0;
+		y++;
+	}
+	ft_printf("\ntotal nbr of points -> %d\n", data->map.points_total);
+	ft_printf("total nbr of coordinates printed -> %d\n", i);
+}
