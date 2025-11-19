54ecfdb311cee8254fb0ff5381c5e1cb639c4f74
Disvster <disaster@selene.localdomain>
2025-11-19 23:10:02 +0000

    fixed map toggling but switching is bugged

--- src/bonus/hooks_bonus.c
+++ src/bonus/hooks_bonus.c
@@ -6,7 +6,7 @@
 /*   By: manmaria <manmaria@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/15 17:03:49 by manmaria          #+#    #+#             */
-/*   Updated: 2025/11/19 23:04:40 by manmaria         ###   ########.fr       */
+/*   Updated: 2025/11/19 23:09:52 by manmaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,9 @@
 
 int	key_press_bonus(int keycode, t_dlist **node)
 {
-	// t_data	*data;
-	//
-	// data = (*node)->data;
-	// if (keycode == 65307)
-	// 	fdf_dlist_clear(node, bonus_wipe_image);
-	// if (keycode == XK_Right)
-	// 	data->keys.right = true;
-	// if (keycode == XK_Left)
-	// 	data->keys.left = true;
-	// if (keycode == XK_q)
-	// 	data->keys.q = true;
-	// if (keycode == XK_e)
-	// 	data->keys.e = true;
-	// if (keycode == XK_h)
-	// 	data->keys.h = true;
-	// if (keycode == XK_j)
-	// 	data->keys.j = true;
-	// if (keycode == XK_k)
-	// 	data->keys.k = true;
-	// if (keycode == XK_l)
-	// 	data->keys.l = true;
-	// return (0);
 	t_data	*data;
 
-	if (!node || !*node)
-		return (0);
-	/* use shared window state to avoid per-node races when switching maps */
-	data = (*node)->window;
-	if (!data)
-		data = (*node)->data;
+	data = (*node)->data;
 	if (keycode == 65307)
 		fdf_dlist_clear(node, bonus_wipe_image);
 	if (keycode == XK_Right)
@@ -67,34 +40,9 @@ int	key_press_bonus(int keycode, t_dlist **node)
 
 int	key_release_bonus(int keycode, t_dlist **node)
 {
-	// t_data	*data;
-	//
-	// data = (*node)->data;
-	// if (keycode == XK_Right)
-	// 	data->keys.right = false;
-	// if (keycode == XK_Left)
-	// 	data->keys.left = false;
-	// if (keycode == XK_q)
-	// 	data->keys.q = false;
-	// if (keycode == XK_e)
-	// 	data->keys.e = false;
-	// if (keycode == XK_h)
-	// 	data->keys.h = false;
-	// if (keycode == XK_j)
-	// 	data->keys.j = false;
-	// if (keycode == XK_k)
-	// 	data->keys.k = false;
-	// if (keycode == XK_l)
-	// 	data->keys.l = false;
-	// return (0);
 	t_data	*data;
 
-	if (!node || !*node)
-		return (0);
-	/* mirror press logic: update the shared window state */
-	data = (*node)->window;
-	if (!data)
-		data = (*node)->data;
+	data = (*node)->data;
 	if (keycode == XK_Right)
 		data->keys.right = false;
 	if (keycode == XK_Left)
@@ -140,50 +88,27 @@ void	fdf_prev_map(t_dlist **node)
 
 void	handle_changes_bonus(t_dlist **node)
 {
-	// t_data	*dtmp1;
-	// t_data	*dtmp2;
-	// t_dlist	*temp;
-	//
-	// temp = *node;
-	// dtmp1 = temp->data;
-	// if (dtmp1->keys.right)
-	// // {
-	// // 	clear_image(dtmp1);
-	// 	fdf_next_map(&temp);
-	// // }
-	// if (dtmp1->keys.left)
-	// // {
-	// // 	clear_image(dtmp1);
-	// 	fdf_prev_map(&temp);
-	// // }
-	// dtmp2 = temp->data;
-	// if (dtmp1->keys.q)
-	// 	dtmp2->view.z_scale -= 0.5;
-	// if (dtmp1->keys.e)
-	// 	dtmp2->view.z_scale += 0.5;
-	// *node = temp;
-	t_data	*dtmp_win; /* keys live in the shared window struct */
-	t_data	*dtmp_map;
+	t_data	*dtmp1;
+	t_data	*dtmp2;
 	t_dlist	*temp;
 
-	if (!node || !*node)
-		return ;
 	temp = *node;
-	/* read keys from the shared window (each node should point to same window) */
-	dtmp_win = temp->window;
-	if (!dtmp_win)
-		dtmp_win = temp->data;
-	/* move pointer based on shared key state */
-	if (dtmp_win->keys.right)
+	dtmp1 = temp->data;
+	if (dtmp1->keys.right)
+	// {
+	// 	clear_image(dtmp1);
 		fdf_next_map(&temp);
-	if (dtmp_win->keys.left)
+	// }
+	if (dtmp1->keys.left)
+	// {
+	// 	clear_image(dtmp1);
 		fdf_prev_map(&temp);
-	/* apply z-scale changes to the (new) active map */
-	dtmp_map = temp->data;
-	if (dtmp_win->keys.q)
-		dtmp_map->view.z_scale -= 0.5;
-	if (dtmp_win->keys.e)
-		dtmp_map->view.z_scale += 0.5;
+	// }
+	dtmp2 = temp->data;
+	if (dtmp1->keys.q)
+		dtmp2->view.z_scale -= 0.5;
+	if (dtmp1->keys.e)
+		dtmp2->view.z_scale += 0.5;
 	*node = temp;
 }
 //
